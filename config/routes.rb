ad3094dfe77bf2dd94545886843b1c55b200a4c3
Minh Vo <minhvo199@gmail.com>
2022-06-30 22:40:17 +0000

    implement user auth

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,23 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-  resources :about, only: [:index]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new, :create]
+    resources :categories, except: [:edit, :update, :show]
   end
+
+  resources :about, only: [:index]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
